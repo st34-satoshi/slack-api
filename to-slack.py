8df2f9f7af8aeca7d872bcdca9c72b43b9de6df8
satoshi-tanaka <st34.satoshi@gmail.com>
2019-12-05 16:22:47 +0900

    able to get channel.history

--- to-slack.py
+++ to-slack.py
@@ -15,8 +15,20 @@ class SlackDriver:
                           params=params)
         print("return ", r.json())
 
+    def get_channel_history(self, channel):
+        # you need to use 'OAuth Access Token' not 'Bot User OAuth Access Token'
+        # channel should be 'id' not 'name'
+        params = {"token": self._token, "channel": channel}
+        header = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+        r = requests.get('https://slack.com/api/channels.history',
+                         headers=header,
+                         params=params)
+        print("return ", r.json())
+
 
 if __name__ == '__main__':
     token = ''  # TODO your token.
     slack = SlackDriver(token)
     slack.send_message("Hello World! from python", "#random")
+    slack.get_channel_history("xxxxx")  # channel should be ID not name
